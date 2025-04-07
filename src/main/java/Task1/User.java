8cb2443709c8b8afb72bfa6526ee9084dc3b0512
TCO932 <chirkof.02@gmail.com>
2025-04-07 21:10:49 +0400

    Task1

--- src/main/java/Task1/User.java
+++ src/main/java/Task1/User.java
@@ -1,40 +1,28 @@
 package Task1;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
+@Data
+@AllArgsConstructor
 public class User {
-    String name;
-    Integer age;
-    String workplace;
-    String address;
-
-    public User(String name, int age, String workplace, String address) {
-        this.name = name;
-        this.age = age;
-        this.workplace = workplace;
-        this.address = address;
-    }
-
-    @Override
-    public String toString() {
-        return name + " (" + age + ")";
-    }
+    private String name;
+    private Integer age;
+    private String workplace;
+    private String address;
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> res = new TreeMap<>();
 
         users.forEach(user -> {
-            List<User> group = res.get(user.age);
-            if (group != null) {
-                group.add(user);
-            } else {
-                ArrayList<User> newGroup = new ArrayList<>();
-                newGroup.add(user);
-                res.put(user.age, newGroup);
-            }
+            List<User> group = res.getOrDefault(user.age, new ArrayList<>());
+            group.add(user);
+            res.putIfAbsent(user.age, group);
         });
 
         return res;
